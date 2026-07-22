c2eed0e06ddb8c5463d733b663b5ef937ce664db
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-07-22 11:53:24 +0200

    add file renaming feature

--- py_simple_package/src/py_simple/easy_file_manager.py
+++ py_simple_package/src/py_simple/easy_file_manager.py
@@ -12,23 +12,9 @@ class InvalidExtension(Exception):
         super().__init__(self.message)
 
 
-def make_blank_file(filename: str, file_extension: str):
-    """
-        Creates a blank file in current working directory.
-
-        Args:
-            filename (str): The name of the file to be created.
-            file_extension (str): The extension of the file to be created without including '.'. Allowed extensions are: ['txt', 'csv', 'md', 'log']
-
-        Example:
-            make_blank_file("new_file", "txt")
-        """
-    if file_extension.lower() in VALID_EXTENSIONS:
-        file = f"{filename}.{file_extension.lower()}"
-        with open(file, 'w', encoding='utf-8'):
-            pass
-    else:
-        raise InvalidExtension(f"\n'{file_extension}' is not a valid extension. Please enter a valid extension and try again.\nVALID EXTENSIONS: {VALID_EXTENSIONS}")
+def is_valid_extension(extension):
+    """Check if extension is valid"""
+    return extension in VALID_EXTENSIONS
 
 
 def is_file_there(filename: str):
@@ -47,6 +33,28 @@ def is_file_there(filename: str):
     return os.path.isfile(filename)
 
 
+def make_blank_file(filename: str, file_extension: str):
+    """
+        Creates a blank file in current working directory. If file already exists, nothing is done.
+
+        Args:
+            filename (str): The name of the file to be created.
+            file_extension (str): The extension of the file to be created without including '.'. Allowed extensions are: ['txt', 'csv', 'md', 'log']
+
+        Example:
+            make_blank_file("new_file", "txt")
+        """
+    if is_valid_extension(file_extension.lower()):
+        file = f"{filename}.{file_extension.lower()}"
+        if is_file_there(file):
+            print(f"File {file} already exists in current working directory.")
+        else:
+            with open(file, 'w', encoding='utf-8'):
+                pass
+    else:
+        raise InvalidExtension(f"\n'{file_extension}' is not a valid extension. Please enter a valid extension and try again.\nVALID EXTENSIONS: {VALID_EXTENSIONS}")
+
+
 def add_a_line(filename: str, line: str):
     """
         Add line to existing file.
@@ -60,7 +68,7 @@ def add_a_line(filename: str, line: str):
             add_a_line("new_file.txt", "hello world!")
         """
     ext = filename.split('.')[-1].lower()
-    if ext in VALID_EXTENSIONS:
+    if is_valid_extension(ext):
         with open(filename, 'a', encoding='utf-8') as f:
             f.write(line + '\n')
     else:
@@ -72,7 +80,7 @@ def read_file_to_list(filename: str):
         Reads lines of existing file to list.
 
         Args:
-            filename (str): File to read from.
+            filename (str): File to read from. Allowed extensions are: ['txt', 'csv', 'md', 'log']
 
         Returns:
             list: List of lines.
@@ -81,7 +89,7 @@ def read_file_to_list(filename: str):
             read_file_to_list("new_file.txt")
         """
     ext = filename.split('.')[-1].lower()
-    if ext in VALID_EXTENSIONS:
+    if is_valid_extension(ext):
         clean_lines = []
         try:
             with open(filename, 'r', encoding='utf-8') as f:
@@ -102,16 +110,43 @@ def remove_file(filename: str):
         Removes file from current working directory.
 
         Args:
-            filename (str): File to delete.
+            filename (str): File to delete. Allowed extensions are: ['txt', 'csv', 'md', 'log']
 
         Example:
             remove_file("new_file.txt")
         """
     ext = filename.split('.')[-1].lower()
-    if ext in VALID_EXTENSIONS:
+    if is_valid_extension(ext):
         if is_file_there(filename):
             os.remove(filename)
         else:
-            print(f"File {filename} does not exist")
+            print(f"File {filename} does not exist!")
     else:
         raise InvalidExtension(f"\n'{ext}' is not a valid extension. Please enter a valid extension and try again.\nVALID EXTENSIONS: {VALID_EXTENSIONS}")
+
+
+def rename_file(old_name: str, new_name: str):
+    """
+        Rename file in the current working directory.
+
+        Args:
+            old_name (str): File to rename. Allowed extensions are: ['txt', 'csv', 'md', 'log']
+            new_name (str): New name for file. Allowed extensions are: ['txt', 'csv', 'md', 'log']
+
+        Example:
+            rename_file("old_name.txt", "new_name.txt")
+        """
+    ext_old = old_name.split('.')[-1].lower()
+    ext_new = new_name.split('.')[-1].lower()
+    if is_valid_extension(ext_old) and is_valid_extension(ext_new):
+        if is_file_there(old_name):
+            if not is_file_there(new_name):
+                os.rename(old_name, new_name)
+            else:
+                print(f"File '{new_name}' already exists in current working directory.")
+        else:
+            print(f"File {old_name} does not exist in current working directory.")
+    else:
+        raise InvalidExtension(
+            f"\n'{ext_old}' or '{ext_new}' is not a valid extension. Please enter a valid extension and try again.\nVALID EXTENSIONS: {VALID_EXTENSIONS}")
+
